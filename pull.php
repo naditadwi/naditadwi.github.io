80d8367413cb8ccccfb009af69a7381a5ca48066
naditadwi <naditaaryanidwi@gmail.com>
2022-07-06 16:42:05 +0700

    versi 1
    v1

---
+++ pull.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($_POST && $_POST['payload']){
+    $payload = ($_POST['payload'], true);
+    if (isset($payload['ref']) && $payload['ref'] == 'refs/heads/master'){
+        exec("/usr/bin/git pull origin master >> ./webhook.log 2>&1 && echo $(date) >> ./webhook.log");
+    }
+}
+
+?>
